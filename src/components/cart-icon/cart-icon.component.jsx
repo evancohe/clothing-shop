77122198ba5af3257c9b1d2a0d959604a8f7956a
evancohe <evsdevsubs@outlook.com>
2021-03-02 23:52:54 +0800

    add new addItem action to cart-action.js

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,11 +7,12 @@ import './cart-icon.styles.scss';
 const CartIcon = ({ toggleCartHidden }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span class="item-count">0</span>
+    <span className="item-count">0</span>
   </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
+
 export default connect(null, mapDispatchToProps)(CartIcon);
