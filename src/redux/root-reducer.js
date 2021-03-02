8aa1103db7935701be4e8adc698ad24be8e82f61
evancohe <evsdevsubs@outlook.com>
2021-03-02 10:38:24 +0800

    add cart reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
-import { combineReducers } from "redux";
-import userReducer from "./user/user.reducer";
+import { combineReducers } from 'redux';
+import cartReducer from './cart/cart.reducer';
+import userReducer from './user/user.reducer';
 
 export default combineReducers({
   //all states related to user
   user: userReducer,
+  cart: cartReducer,
 });
