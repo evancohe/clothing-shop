fe51a8df63c1c0ce588c0a9dd2d83a53d1dfd8f0
evancohe <evsdevsubs@outlook.com>
2021-03-10 17:11:25 +0800

    added local storage by using redux-persist

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,7 +7,6 @@ import {
   selectCartTotal,
 } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-import CartItem from '../../components/cart-item/cart-item.component';
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
